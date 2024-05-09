f36e873973d29a0792abaf87e71977bcb4ededf2
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2024-05-09 20:06:40 -0300

    Add new script and mock data

--- src/index.js
+++ src/index.js
@@ -5,38 +5,6 @@ import { FormController } from "./FormController";
 import { ScreenController } from "./screenController";
 import "./style.css";
 
-const mock = {
-  task1: {
-    title: "dishes",
-    description: "do the dishes",
-
-    priority: 3,
-  },
-  task2: {
-    title: "study",
-    description: "math today",
-
-    priority: 1,
-  },
-  task3: {
-    title: "walk dog",
-    description: "boo",
-
-    priority: 2,
-    project: { type: "name", projectName: "Home" },
-  },
-};
-
 app.init();
-// const myTodo = app.addNewTodo(mock.task1);
-// app.addNewTodo(mock.task2);
-// app.addNewTodo(mock.task3);
-// app.findAndToggleCompletedTodo(1);
-// console.log("list projects:");
-// app.listProjects((project) => console.log(project.toString()));
-// console.log(app.projects);
-// console.log("testing the list todos");
-// console.log(app.findTodoById(0));
-ScreenController.renderAllProjects();
 
-console.log(app.projects);
+ScreenController.renderAllProjects();
